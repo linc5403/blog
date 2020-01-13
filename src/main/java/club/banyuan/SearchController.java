cf938c1737d917aaf83a37e614cbad3e484592be
linchuan <linc1982@icloud.com>
2020-01-13 15:40:09 +0800

    mybatis init

--- src/main/java/club/banyuan/SearchController.java
+++ src/main/java/club/banyuan/SearchController.java
@@ -2,14 +2,14 @@ package club.banyuan;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ResponseBody
-public class IndexController {
-    @GetMapping("/user/{username}")
-    String getUserBlogs(@PathVariable String username) {
-        return username;
+public class SearchController {
+    @GetMapping("/search")
+    String searchBlog(@RequestParam String key) {
+        return "search key is " + key;
     }
 }
---
+++ src/main/java/club/banyuan/bean/PageInfo.java
@@ -0,0 +1,23 @@
+package club.banyuan.bean;
+
+public class PageInfo {
+    private Integer page;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    private Integer size;
+}
