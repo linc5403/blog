8396bdd17a91b4fb8b0f7280bf23a3ee0e8b68ee
linchuan <linc1982@icloud.com>
2020-01-17 11:38:40 +0800

    process chinese redirect

--- src/main/java/club/banyuan/controller/LoginController.java
+++ src/main/java/club/banyuan/controller/LoginController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -27,10 +30,12 @@ public class LoginController {
     }
 
     @PostMapping
-    String login(
+    void login(
+            HttpServletRequest request,
+            HttpServletResponse response,
             @RequestParam String username,
             @RequestParam String password,
-            HttpSession session) throws UnsupportedEncodingException {
+            HttpSession session) throws IOException {
         // 1. get form data(username password)
         // 2. dao -> username:password
         User user = userService.findUserByName(username);
@@ -43,15 +48,18 @@ public class LoginController {
             // 4.1 如果带有next参数，重定向到next
             String nextUri = (String)session.getAttribute("NEXT_URI");
             if (nextUri != null) {
-                return "redirect:" + URLEncoder.encode(nextUri, "UTF-8");
+                String redirectUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + nextUri;
+                response.setHeader("Location", redirectUrl);
+                response.setStatus(302);
+//                return "redirect:" + URLEncoder.encode(redirectUrl, "UTF-8");
             }
             // 4.2 不带next表示直接访问的login，那么跳转到user的controller
-            return "redirect:/user/" + URLEncoder.encode(username, "UTF-8");
+            response.sendRedirect("/user/" + URLEncoder.encode(username, "UTF-8"));
+//            return "redirect:/user/" + URLEncoder.encode(username, "UTF-8");
             // return "redirect:" + URLEncoder.encode("/user/" + username, "UTF-8");
         }
         else {
             // login fail
         }
-        return "";
     }
 }
